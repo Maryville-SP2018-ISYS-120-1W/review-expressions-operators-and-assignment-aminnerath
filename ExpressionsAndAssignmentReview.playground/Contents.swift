1177a5b1b7f0e5e1be2db5f1e811d97ad9d6f5f4
aminnerath <35513428+aminnerath@users.noreply.github.com>
2018-01-29 20:55:31 -0600

    Update Contents.swift

--- ExpressionsAndAssignmentReview.playground/Contents.swift
+++ ExpressionsAndAssignmentReview.playground/Contents.swift
@@ -14,4 +14,40 @@
  You should write your story bits in comments (e.g., between /* and */, or lines starting with // ) around where the code should go.
  
 */
+//Jeff and Andrea have seperate accounts and they keep a cash account at home.  One account has 100 dollars in it and another account has 200. At home they keep 500 dollats.  Create a budget of how much they have to pay bills with.
 
+var bank1:Double=100
+var bank2:Double=200
+var cash:Double=500
+let total=bank1+bank2+cash
+//The best advice is to pay bills first then plan trip.  Print this advice.
+print("Pay bills first,then plan trip")
+//The bills to be paid are a credit card and a student loan.  The credit card payment is 50 and the student loan is 100.
+// print the total bill amount
+var creditPayment=50
+var studentLoan=100
+var bills=creditPayment+studentLoan
+print(bills)
+//Since you should always pay more than minium due you sometimes need a reminder.  Print why and a reminder
+print("Pay more down to avoid interest")
+var bills=(creditPayment+studentLoan)*2
+print(bills)
+//what does this amount mean?  print this answer
+Print("This is how much to budget out to pay everything that is due!")
+Print(total
+// Be sarcastic and print a statement
+Print("Look at all the cash")
+Var paybill:Double=total-bills
+Print(paybill)
+//How do you think Jeff and Andrea feel after paying bills? Print your answer
+Print("Ouch!That sure went fast")
+//Andrea is signed up for cash back rewards.  However only one account has this option.  Take the lowest account multiple by 2 and add 50.
+Print("Cash back rewards came. Add to bank1")
+bank1=(100*2)+50
+ // Sometimes things come up unexpected in a budget. Today the kids needed lunch. The lunch is 20.  Adjust accordingly and print reason
+Print("Kids need lunch.")
+var lunch=20
+bank1=(100*2)+50 -lunch
+print (paybill)
+// What do you think was the largest influence in the budget?  Print your answer
+print("It pays to do cash back rewards")
